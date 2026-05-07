fb0ec6598b1bee04db19d4f4ce869fa5753c9eb7
Fahmida0010 <fahmidaakter0010@gmail.com>
2026-05-07 15:02:14 +0600

    first commit

---
+++ solutions.ts
@@ -0,0 +1,12 @@
+//problem 1 //
+
+
+function filterEvenNumbers(numbers: number[]) {
+   return numbers.filter((num) => num % 2 === 0);
+}
+
+ const result = filterEvenNumbers([1, 2, 3, 4, 5, 6])
+   console.log(result);
+
+   //problem 2//
+   
